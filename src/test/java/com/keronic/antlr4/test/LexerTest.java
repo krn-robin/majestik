7c3dd82d76efdc427ded23c79b5086c77eb05a79
Robin Putters <robin.putters@keronic.com>
2024-07-31 14:18:38 +0200

    googleJavaFormat

--- src/test/java/com/keronic/antlr4/test/LexerTest.java
+++ src/test/java/com/keronic/antlr4/test/LexerTest.java
@@ -2,10 +2,10 @@ package com.keronic.antlr4.test;
 
 import static org.junit.Assert.*;
 
+import com.keronic.antlr4.MajestikLexer;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.IntStream;
-
 import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStreams;
@@ -15,14 +15,18 @@ import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.Token;
 import org.junit.Test;
 
-import com.keronic.antlr4.MajestikLexer;
-
 public class LexerTest {
-	ANTLRErrorListener errorListener = new BaseErrorListener() {
+  ANTLRErrorListener errorListener =
+      new BaseErrorListener() {
 
 		@Override
-		public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
-				String msg, RecognitionException e) {
+        public void syntaxError(
+            Recognizer<?, ?> recognizer,
+            Object offendingSymbol,
+            int line,
+            int charPositionInLine,
+            String msg,
+            RecognitionException e) {
 			fail(String.format("%i %i %s %n", line, charPositionInLine, msg, offendingSymbol));
 		}
 	};
@@ -39,12 +43,13 @@ public class LexerTest {
 
 	private void compareTokens(int[] expected, List<Token> actual) {
 		assertEquals(expected.length, actual.size());
-		IntStream.range(0, expected.length).forEach(n -> assertEquals(expected[n], actual.get(n).getType()));
+    IntStream.range(0, expected.length)
+        .forEach(n -> assertEquals(expected[n], actual.get(n).getType()));
 	}
 
 	@Test
 	public void testAssignString() throws IOException {
-		final var expected = new int[] { MajestikLexer.VAR, MajestikLexer.ASSIGN, MajestikLexer.STRING };
+    final var expected = new int[] {MajestikLexer.VAR, MajestikLexer.ASSIGN, MajestikLexer.STRING};
 		var tokens = this.getTokensFromText("var << \"value\"");
 
 		this.compareTokens(expected, tokens);
@@ -52,7 +57,7 @@ public class LexerTest {
 
 	@Test
 	public void testBlock() throws IOException {
-		final var expected = new int[] { MajestikLexer.BLOCK, MajestikLexer.ENDBLOCK };
+    final var expected = new int[] {MajestikLexer.BLOCK, MajestikLexer.ENDBLOCK};
 		var tokens = this.getTokensFromText("_block _endblock");
 
 		this.compareTokens(expected, tokens);
@@ -60,7 +65,8 @@ public class LexerTest {
 
 	@Test
 	public void testCaseInsensitive() throws IOException {
-		final var expected = new int[] { MajestikLexer.ENDBLOCK, MajestikLexer.ENDBLOCK, MajestikLexer.ENDBLOCK };
+    final var expected =
+        new int[] {MajestikLexer.ENDBLOCK, MajestikLexer.ENDBLOCK, MajestikLexer.ENDBLOCK};
 		var tokens = this.getTokensFromText("_EndBlock _ENDBLOCK _eNDBLOCk");
 
 		this.compareTokens(expected, tokens);
@@ -68,7 +74,7 @@ public class LexerTest {
 
 	@Test
 	public void testEmptyStringDoubleQuote() throws IOException {
-		final var expected = new int[] { MajestikLexer.STRING };
+    final var expected = new int[] {MajestikLexer.STRING};
 		var tokens = this.getTokensFromText("\"\"");
 
 		this.compareTokens(expected, tokens);
@@ -76,7 +82,7 @@ public class LexerTest {
 
 	@Test
 	public void testEmptyStringSingleQuote() throws IOException {
-		final var expected = new int[] { MajestikLexer.STRING };
+    final var expected = new int[] {MajestikLexer.STRING};
 		var tokens = this.getTokensFromText("''");
 
 		this.compareTokens(expected, tokens);
@@ -84,7 +90,7 @@ public class LexerTest {
 
 	@Test
 	public void testNumberLong() throws IOException {
-		final var expected = new int[] { MajestikLexer.NUMBER };
+    final var expected = new int[] {MajestikLexer.NUMBER};
 		var tokens = this.getTokensFromText("54321");
 
 		this.compareTokens(expected, tokens);
@@ -92,7 +98,7 @@ public class LexerTest {
 
 	@Test
 	public void testNumberDouble() throws IOException {
-		final var expected = new int[] { MajestikLexer.NUMBER };
+    final var expected = new int[] {MajestikLexer.NUMBER};
 		var tokens = this.getTokensFromText("5.4321");
 
 		this.compareTokens(expected, tokens);
@@ -100,7 +106,7 @@ public class LexerTest {
 
 	@Test
 	public void testVariable() throws IOException {
-		final var expected = new int[] { MajestikLexer.VAR };
+    final var expected = new int[] {MajestikLexer.VAR};
 		var tokens = this.getTokensFromText("var");
 
 		this.compareTokens(expected, tokens);
