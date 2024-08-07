b98d1935b4c6d149e28f50dd4f0cc2b071b9c5aa
Robin Putters <robin.putters@keronic.com>
2024-08-07 19:39:45 +0200

    Add class BinaryDispatcher

---
+++ src/main/java/com/keronic/majestik/language/invokers/BinaryDispatcher.java
@@ -0,0 +1,11 @@
+package com.keronic.majestik.language.invokers;
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+public class BinaryDispatcher {
+  public static CallSite bootstrap(MethodHandles.Lookup lookup, String name, MethodType type) {
+    return null;
+  }
+}
