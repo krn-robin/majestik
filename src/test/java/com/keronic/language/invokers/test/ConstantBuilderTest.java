edbc3906c17e43b6555122e64e659a624826253c
Robin Putters <robin.putters@keronic.com>
2024-08-11 23:06:01 +0200

    FInish test for ConstantBuilder

--- src/test/java/com/keronic/language/invokers/test/ConstantBuilderTest.java
+++ src/test/java/com/keronic/language/invokers/test/ConstantBuilderTest.java
@@ -5,6 +5,7 @@ import static java.lang.classfile.ClassFile.ACC_STATIC;
 import static org.junit.Assert.*;
 
 import com.keronic.majestik.constant.ConstantDescs;
+import com.keronic.majestik.runtime.internal.ProcImpl;
 import java.lang.classfile.ClassFile;
 import java.lang.classfile.CodeBuilder;
 import java.lang.constant.ClassDesc;
@@ -15,6 +16,9 @@ import java.util.function.Consumer;
 import org.junit.Test;
 
 public class ConstantBuilderTest {
+  /**
+   * @throws Throwable
+   */
   @Test
   public void testStringBuilder() throws Throwable {
     var mt = MethodType.methodType(String.class);
@@ -41,5 +45,53 @@ public class ConstantBuilderTest {
     var lookup = MethodHandles.lookup().defineHiddenClass(bytes, true);
     var m = lookup.findStatic(lookup.lookupClass(), "m", mt);
     assertEquals(m.invoke(), str);
+  }
+
+  public static Object testM() {
+    return Integer.valueOf(12345);
+  }
+  ;
+
+  /**
+   * @throws Throwable
+   */
+  @Test
+  public void testProcWithEmptyEnvBootstrap() throws Throwable {
+    var mt = MethodType.methodType(ProcImpl.class);
+    var mtd = mt.describeConstable().get();
+    var classname = ClassDesc.of(this.getClass().getName());
+
+    Consumer<CodeBuilder> cb =
+        xb -> {
+          xb.invokedynamic(
+              DynamicCallSiteDesc.of(
+                  ConstantDescs.BSM_PROC_EMPTY_ENV_LITERAL,
+                  "proc",
+                  ConstantDescs.MTD_Object,
+                  classname,
+                  "testM",
+                  "test_method",
+                  0,
+                  0,
+                  0,
+                  0));
+          xb.checkcast(ClassDesc.of(ProcImpl.class.getName()));
+          xb.areturn();
+        };
+
+    var bytes =
+        ClassFile.of()
+            .build(
+                ClassDesc.of("com.keronic.language.invokers.test.C"),
+                clb -> {
+                  clb.withMethodBody("m", mtd, ACC_PUBLIC | ACC_STATIC, cb);
+                });
+
+    var lookup = MethodHandles.lookup().defineHiddenClass(bytes, true);
+    var m = lookup.findStatic(lookup.lookupClass(), "m", mt);
+    System.out.format("====> %s%n", m.invoke());
+    var result = (ProcImpl) m.invoke();
+    assertNotNull(result);
+    assertEquals(result.invoke(), 12345);
   }
 }
