a133d152a0faa21498a51d117d5057278e7c4282
Robin Putters <robin.putters@keronic.com>
2024-09-20 00:11:04 +0200

    Add extra tests

--- src/test/java/com/keronic/PathUtilsTest.java
+++ src/test/java/com/keronic/PathUtilsTest.java
@@ -11,4 +11,19 @@ class PathUtilsTest {
   void testGetBaseName() {
 		assertEquals("test", PathUtils.getBaseName("test.majestik"));
 	}
+
+  @Test
+  void testGetBaseNameDoubleExt() {
+    assertEquals("test.new", PathUtils.getBaseName("test.new.majestik"));
+  }
+
+  @Test
+  void testGetBaseNameEmpty() {
+    assertEquals("", PathUtils.getBaseName(""));
+  }
+
+  @Test
+  void testGetBaseNameSpace() {
+    assertEquals("test test", PathUtils.getBaseName("test test.ext"));
+  }
 }
