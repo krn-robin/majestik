0e6666f23f0a3b314e31ad38fb2f4fae2aa8f80a
Robin Putters <robin.putters@keronic.com>
2024-11-15 22:23:34 +0100

    Remove debug code

--- src/main/java/com/keronic/MajestikCodeVisitor.java
+++ src/main/java/com/keronic/MajestikCodeVisitor.java
@@ -6,7 +6,6 @@ import com.keronic.majestik.constant.ConstantDescs;
 import com.sonar.sslr.api.AstNode;
 import java.lang.System.Logger.Level;
 import nl.ramsolutions.sw.magik.MagikVisitor;
-import nl.ramsolutions.sw.magik.api.MagikGrammar;
 
 public class MajestikCodeVisitor extends MagikVisitor {
   private static final System.Logger LOGGER =
@@ -28,13 +27,27 @@ public class MajestikCodeVisitor extends MagikVisitor {
   }
 
   @Override
-  protected void walkPreString(AstNode node) {
-    String quotedString = node.getTokenValue();
-    String pureString = normalizeString(quotedString);
+  protected void walkPostAssignmentExpression(final AstNode node) {
+    var varname = node.getTokenValue();
 
-    this.cb.invokedynamic(
-        DynamicCallSiteDesc.of(
-            ConstantDescs.BSM_STRING_BUILDER, "string", ConstantDescs.MTD_Object, pureString));
+    if (!this.varMap.containsKey(varname)) this.varMap.put(varname, this.varMap.size());
+
+    this.cb.astore(this.varMap.getOrDefault(varname, -1));
+  }
+
+  @Override
+  protected void walkPreIdentifier(final AstNode node) {
+    var varname = node.getTokenValue();
+    var varidx = this.varMap.get(varname);
+    if (varidx == null)
+      this.cb.invokedynamic(
+          DynamicCallSiteDesc.of(
+              ConstantDescs.BSM_GLOBAL_FETCHER,
+              "fetch",
+              ConstantDescs.MTD_Object,
+              "sw",
+              node.getParent().getTokenValue()));
+    else this.cb.aload(varidx);
   }
 
   @Override
@@ -56,70 +69,23 @@ public class MajestikCodeVisitor extends MagikVisitor {
   }
 
   @Override
-  protected void walkPreProcedureInvocation(AstNode node) {
-    System.out.println("LOG: - Compiling invoke...");
-
-    this.cb.invokedynamic(
-        DynamicCallSiteDesc.of(
-            ConstantDescs.BSM_GLOBAL_FETCHER,
-            "fetch",
-            ConstantDescs.MTD_Object,
-            "sw",
-            node.getParent().getTokenValue()));
-  }
-
-  @Override
   protected void walkPostProcedureInvocation(AstNode node) {
     // https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/lang/invoke/CallSite.html
 
-    System.out.println("LOG: - Compiling invoke...");
     this.cb.invokedynamic(
         DynamicCallSiteDesc.of(
             ConstantDescs.BSM_NATURAL_PROC, "()", ConstantDescs.MTD_ObjectObjectObject));
   }
 
   @Override
-  protected void walkPreAtom(AstNode node) {
-    var nodeType = ((MagikGrammar) node.getType()).name();
-    var nodeText = node.getTokenValue();
-    System.out.format("LOG: - Enter %s: %s%n", nodeType, nodeText);
+  protected void walkPreString(AstNode node) {
+    String quotedString = node.getTokenValue();
+    String pureString = normalizeString(quotedString);
+
+    this.cb.invokedynamic(
+        DynamicCallSiteDesc.of(
+            ConstantDescs.BSM_STRING_BUILDER, "string", ConstantDescs.MTD_Object, pureString));
   }
-
-  @Override
-  protected void walkPreDefault(AstNode node) {
-    var nodeType = ((MagikGrammar) node.getType()).name();
-    System.out.format("LOG: - Enter %s%n", nodeType);
-  }
-
-  @Override
-  protected void walkPostDefault(AstNode node) {
-    var nodeType = ((MagikGrammar) node.getType()).name();
-    System.out.format("LOG: - Leave %s%n", nodeType);
-  }
-
-  /*
-
-  @Override
-  public Void visitVar(MajestikParser.VarContext ctx) {
-  	if (ctx.parent instanceof MajestikParser.ArgumentContext) {
-  		var varname = ctx.getText();
-  		this.cb.aload(this.varMap.getOrDefault(varname, -1));
-  	}
-  	return visitChildren(ctx);
-  }
-
-  @Override
-  public Void visitAssign(MajestikParser.AssignContext ctx) {
-  	var result = visitChildren(ctx);
-
-  	var varname = ctx.lhs().var().getText();
-
-  	if (!this.varMap.containsKey(varname))
-  		this.varMap.put(varname, this.varMap.size());
-
-  	this.cb.astore(this.varMap.getOrDefault(varname, -1));
-  	return result;
-  } */
 
   private String normalizeString(String quotedString) {
     assert quotedString.length() >= 2 : "String length is too short to be valid.";
