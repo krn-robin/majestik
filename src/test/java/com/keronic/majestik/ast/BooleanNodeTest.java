3b7a6c0727f60370cc2cb4d7b9c40537e8f06916
Robin Putters <robin.putters@keronic.com>
2024-12-17 16:17:42 +0100

    Fix a329e2d2350a51d051d8f2e114dc671859dae26a

--- src/test/java/com/keronic/majestik/ast/BooleanNodeTest.java
+++ src/test/java/com/keronic/majestik/ast/BooleanNodeTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class BooleanNodeTest extends NodeTest {
